0e5853c8c8ed3ac7bb7f43c9835ccc140840abc4
krishna <krishna.fnatic@gmail.com>
2020-05-09 19:22:36 +0530

    minor fix

--- resources/views/admin/analytics.blade.php
+++ resources/views/admin/analytics.blade.php
@@ -3,6 +3,30 @@
 
 @section('content')
 
+<div class="row">
+	
+	<div class="col">
+		<div class="info-box">
+        <!-- Apply any bg-* class to to the icon to color it -->
+        <span class="info-box-icon bg-blue"><i class="fas fa-align-justify"></i></span>
+        <div class="info-box-content">
+          <span class="info-box-text">ORDERS</span>
+          <span class="info-box-number">93,139</span>
+        </div><!-- /.info-box-content -->
+      </div><!-- /.info-box -->
+	</div>
+	<div class="col">
+		<div class="info-box">
+        <!-- Apply any bg-* class to to the icon to color it -->
+        <span class="info-box-icon bg-green"><i class="fas fa-rupee-sign"></i></span>
+        <div class="info-box-content">
+          <span class="info-box-text">TOTAL REVENUE</span>
+          <span class="info-box-number">₹ 1,93,139</span>
+        </div><!-- /.info-box-content -->
+      </div><!-- /.info-box -->
+	</div>
+
+</div>
 
 @endsection
 
